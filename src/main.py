ccb4b88914dda8773e735ed0f703944793548f16
Alex Bakker <alex.bakker@energyworx.org>
2022-10-21 10:03:57 +0200

    add hello endpoint

--- src/main.py
+++ src/main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return 'Hello!', 200, {}
 
+@app.route('/hello')
+def hello():
+    return 'Hello world!', 200, {}
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
